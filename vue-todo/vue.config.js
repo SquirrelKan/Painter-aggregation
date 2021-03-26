7d698c8c6209610b585da056c80fdb0f5893b006
Andrew.Chi <andrew.chi@collaborate..tw>
2021-03-26 18:11:51 +0800

    Update .gitignore, package-lock.json, and 60 more files...

--- vue-todo/vue.config.js
+++ vue-todo/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://10.0.4.29:8090',
+          target: 'http://127.0.0.1:8090',
           changeOrigin: true
         }
       }
---
+++ webapicore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace webapicore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       
+
+        // GET: HomeController1
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var message = "Alochol is important.";
+
+            _logger.LogInformation(message);
+
+            ViewBag.MessageValue = message;
+
+            return View();
+        }
+    }
+}
