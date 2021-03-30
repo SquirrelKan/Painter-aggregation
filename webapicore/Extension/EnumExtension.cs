24cdf64c288307c6744144156ad3b986c6870972
Andrew.Chi <andrew.chi@collaborate..tw>
2021-03-30 17:07:24 +0800

    Update excel.html, logo.png, and 12 more files...

---
+++ webapicore/Extension/EnumExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace webapicore.Extension
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription<T>(this T enumValue)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                return null;
+            }
+
+            var description = enumValue.ToString();
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    description = ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            return description;
+        }
+    }
+}
---
+++ webapicore/Services/Interface/IMemberService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapicore.Models;
+
+namespace webapicore.Services.Interface
+{
+    public interface IMemberService
+    {
+        Task<List<Member>> GetMember();
+        Task<Member> GetUserByUserNamePassWord(string userName, string passWord);
+        void Register(UserInfo userInfo);
+    }
+}
